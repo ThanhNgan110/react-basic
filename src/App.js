6b6d132f0b86e6cdd0b5eabaa4035bfbc62c379c
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2023-05-26 23:29:24 +0700

    ngan

--- src/App.js
+++ src/App.js
@@ -13,17 +13,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Thanh Ngan </h2>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <MyComponent>
 
         </MyComponent>
--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,25 +1,40 @@
 import React from "react";
 
 class MyComponent extends React.Component {
-    render() {
-        let name = 'Thanh Ngan';
-        let birthday = '01-10-2002';
 
-
-        return (
-            <div className="wrapper">
-
-                <div>
-                    {console.log('My name is', name)}
-
-                    hello, {name}</div>
-                <div>Age {birthday}</div>
-            </div>
-
-        )
-
+    state = {
+        name: '',
+        age: 21
     }
 
+    handleClickButton = () => {
+        console.log('Hello');
+    }
+
+    handleOnchangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    render() {
+        console.log('Call render', this.state);
+        return (
+            <>
+                <div>
+                    {console.log('My name is', this.state.name)}
+                    hello, {this.state.name}
+                </div>
+                <div>Age {this.state.age}</div>
+                <div className="third">
+                    <button onClick={this.handleClickButton}>Click me</button>
+                </div>
+                <div>
+                    <input type="text" value={this.state.name} onChange={(event) => this.handleOnchangeName(event)} />
+                </div>
+            </>
+        );
+    }
 }
 
 export default MyComponent;
